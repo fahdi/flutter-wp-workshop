795574496f86aa41f13692bf4f4ce7284050114d
Fahad Murtaza <fahdim@gmail.com>
2019-09-14 08:50:18 +0400

    Commit before leaving for dubai.

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/fahadmurtaza/Code/flutter-core"
+export "FLUTTER_APPLICATION_PATH=/Users/fahadmurtaza/Code/flutter_wordpress/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/fahadmurtaza/Code/flutter-core/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+void main() {
+  runApp(WordPressApp());
+}
+
+class WordPressApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WordPress Demo',
+      theme: ThemeData.light(),
+      home: LoginPage(),
+    );
+  }
+}
+
+
